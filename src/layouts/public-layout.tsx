31d115186e3677a0d11d373eb8f604d74ec200e9
Vaibhavi Bhaskar Palande <vibha.palande5@gmail.com>
2025-05-16 18:20:33 +0530

    Changes: Firebase configuration and user data saves

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/header"
 import Footer from "@/components/footer"
 import { Outlet } from "react-router-dom"
+import AuthHandler from "@/handlers/auth-handler"
 
 export const PublicLayout = () => {
   return (
     <div className='w-full'>
+      <AuthHandler />
       <Header />
 
       <Outlet/>
